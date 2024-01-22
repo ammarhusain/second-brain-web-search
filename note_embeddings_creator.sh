ad9ff1a9a1408ec01a38601c23f508e35ac9ecbc
Ammar Husain <ammarh@t686>
2024-01-21 19:56:38 -0800

    updated scripted to activate conda env after miniforge disruption

--- note_embeddings_creator.sh
+++ note_embeddings_creator.sh
@@ -6,7 +6,7 @@ source ~/.bash_profile
 
 eval "$(conda shell.bash hook)"
 
-conda activate milvus
+conda activate /Users/ammarh/anaconda3/envs/milvus
 
 python /Users/ammarh/Documents/second-brain/vault-management/second-brain-web-search/note_embeddings_creator.py
 
